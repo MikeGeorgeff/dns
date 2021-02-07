64c5b5d806a2ab52e15b18de2923beaaa7b8fbab
Mike Georgeff <mikegeorgeff3@gmail.com>
2021-02-06 21:14:15 -0600

    assign static ip

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bon/bash
 
-docker run -d --name=dns -v $(pwd)/config:/root -p 53:53/udp --restart=always coredns/coredns -conf /root/Corefile
+docker run -d --name=dns -v $(pwd)/config:/root -p 53:53/udp --restart=always --network=dns-internal --ip=172.30.0.10 coredns/coredns -conf /root/Corefile
