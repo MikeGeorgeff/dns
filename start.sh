7cadfcef9756e7d48ff55fe75ba42fb53f61643f
Mike Georgeff <mike.georgeff@uscca.com>
2021-02-06 18:55:31 -0600

    fix

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bon/bash
 
-docker run -d --name=dns -v $(pwd)/config:/root -p 53:53/udp coredns/coredns --restart=always -conf /root/Corefile
+docker run -d --name=dns -v $(pwd)/config:/root -p 53:53/udp --restart=always coredns/coredns -conf /root/Corefile
