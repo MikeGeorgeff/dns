0d1bdef3282b987995085f389da03ce380470faf
Mike Georgeff <mike.georgeff@uscca.com>
2021-02-08 21:37:54 -0600

    fix typo

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
-#!/bon/bash
+#!/bin/bash
 
 docker run -d --name=dns -v $(pwd)/config:/root -p 53:53/udp --restart=always --network=dns-internal --ip=172.30.0.10 coredns/coredns -conf /root/Corefile
